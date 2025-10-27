3aad75486067ba9b64fce2479900e14c2be9c8e5
Ayush Sharma <157533566+ayush2723@users.noreply.github.com>
2025-10-27 23:19:20 +0530

    Minor Changes

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -2,7 +2,7 @@ import appointment_img from './appointment_img.png'
 import header_img from './header_img.png'
 import group_profiles from './group_profiles.png'
 import profile_pic from './profile_pic.png'
-import contact_image from './contact_image.png'
+import contact_image from './contact_image.jpg'
 import about_image from './about_image.png'
 import logo from './logo.svg'
 import dropdown_icon from './dropdown_icon.svg'
@@ -13,23 +13,22 @@ import verified_icon from './verified_icon.svg'
 import arrow_icon from './arrow_icon.svg'
 import info_icon from './info_icon.svg'
 import upload_icon from './upload_icon.png'
-import stripe_logo from './stripe_logo.png'
-import razorpay_logo from './razorpay_logo.png'
-import doc1 from './doc1.png'
-import doc2 from './doc2.png'
-import doc3 from './doc3.png'
-import doc4 from './doc4.png'
-import doc5 from './doc5.png'
-import doc6 from './doc6.png'
-import doc7 from './doc7.png'
-import doc8 from './doc8.png'
-import doc9 from './doc9.png'
-import doc10 from './doc10.png'
-import doc11 from './doc11.png'
-import doc12 from './doc12.png'
-import doc13 from './doc13.png'
-import doc14 from './doc14.png'
-import doc15 from './doc15.png'
+
+import doc1 from './doc1.jpg'
+import doc2 from './doc2.jpg'
+import doc3 from './doc3.jpg'
+import doc4 from './doc4.jpg'
+import doc5 from './doc5.jpg'
+import doc6 from './doc6.jpg'
+import doc7 from './doc7.jpg'
+import doc8 from './doc8.jpg'
+import doc9 from './doc9.jpg'
+import doc10 from './doc10.jpg'
+import doc11 from './doc11.jpg'
+import doc12 from './doc12.jpg'
+import doc13 from './doc13.jpg'
+import doc14 from './doc14.jpg'
+import doc15 from './doc15.jpg'
 import Dermatologist from './Dermatologist.svg'
 import Gastroenterologist from './Gastroenterologist.svg'
 import General_physician from './General_physician.svg'
@@ -54,8 +53,7 @@ export const assets = {
     cross_icon,
     dropdown_icon,
     upload_icon,
-    stripe_logo,
-    razorpay_logo
+ 
 }
 
 export const specialityData = [
